f8dfd6e9a34f32bbb3caef1648259b94ef70b9e8
Jack Busa <jbusa@umich.edu>
2020-04-20 16:00:30 -0400

    Fix incorrect buildpath

--- umemes-server/server.js
+++ umemes-server/server.js
@@ -55,11 +55,11 @@ function getQuery(query) {
 }
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'umemes-client/build')));
     
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'umemes-client/build', 'index.html'));
   });
 }
 
